d471bdcd82e5b800cd4e086e19b109492525fb5f
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2022-06-21 08:46:21 -0300

    Alterando nome da pasta e informações dentro dos arquivos

---
+++ src/pages/Customers/index.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import ButtonAdd from '../../components/ButtonAdd';
+
+const Customers = () => {
+  const navigate = useNavigate();
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    async function getCustomers() {
+      const response = await axios.get('https://localhost:7097/customers');
+      setCustomers(response.data);
+    }
+    getCustomers();
+  }, []);
+
+  function add() {
+    navigate('/customersform');
+  }
+
+  if (customers.length !== 0) {
+    return (
+      <div>
+        <h1>Lista de clientes</h1>
+
+        <div>
+          <table style={{ borderStyle: 'solid' }}>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>CPF</th>
+                <th>Contato</th>
+                <th>Data de Nascimento</th>
+                <th>Opções</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {customers.map((customer) => (
+                <tr key={customer.id}>
+                  <td>{customer.name}</td>
+                  <td>{customer.email}</td>
+                  <td>{customer.cpf}</td>
+                  <td>{customer.contact}</td>
+                  <td>{customer.birthDate}</td>
+                  <td>{}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <ButtonAdd add={add} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Carregando...</p>
+      </div>
+    );
+  }
+};
+
+export default Customers;
