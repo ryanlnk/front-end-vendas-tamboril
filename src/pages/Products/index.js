096d40b0af04099542a6dca790a448e2408ed282
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2022-07-20 22:21:36 -0300

    Adicionando a página de produtos

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,107 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import ButtonAdd from '../../components/ButtonAdd';
+import ButtonRemove from '../../components/ButtonRemove';
 
 const Products = () => {
-  return (
-    <>
-      <h1 className="ml-4 text-2xl font-bold">Produtos</h1>
-    </>
-  );
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await axios.get(
+        'https://localhost:7097/products?_expand=category',
+      );
+      setProducts(response.data);
+    }
+    getProducts();
+  }, []);
+
+  async function deleteProduct(id) {
+    await axios.delete(`https://localhost:7097/products/${id}`);
+    setProducts(products.filter((product) => product.id !== id));
+  }
+
+  function add() {
+    navigate('/productsform');
+  }
+
+  if (products.length !== 0) {
+    return (
+      <>
+        <h1 className="ml-4 text-2xl font-bold">Produtos</h1>
+
+        <ButtonAdd add={add} />
+
+        <table className="m-5 w-5/6 mx-auto table-auto">
+          <thead>
+            <tr className="text-left border-b border-gray-300">
+              <th className="px-4 py-1">Nome</th>
+              <th className="px-4 py-1">Preço</th>
+              <th className="px-4 py-1">Quantidade</th>
+              <th className="px-4 py-1">Categoria</th>
+              <th className="px-4 py-1">Opções</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {products.map((product) => (
+              <tr
+                className="border-b border-gray-200 hover:bg-gray-100"
+                key={product.id}
+              >
+                <td className="px-4 py-2">{product.name}</td>
+                <td className="px-4 py-2">R$ {product.salePrice}</td>
+                <td className="px-4 py-2">{product.quantity}</td>
+                <td className="px-4 py-2">{product.category.name}</td>
+                <td className="px-4 py-2 inline-flex">
+                  {
+                    <Link
+                      className="text-indigo-800 hover:text-indigo-200 hover:scale-125 active:scale-95 ease-in-out duration-100"
+                      to={`/productsform/${product.id}`}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                        />
+                      </svg>
+                    </Link>
+                  }
+                  <ButtonRemove
+                    deleteFunction={deleteProduct}
+                    parameter={product.id}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1 className="ml-4 text-2xl font-bold">Produtos</h1>
+
+        <div>
+          <ButtonAdd add={add} />
+        </div>
+
+        <p className="ml-4">Carregando...</p>
+      </>
+    );
+  }
 };
 
 export default Products;
--- src/pages/ProductsForm/index.js
+++ src/pages/ProductsForm/index.js
@@ -1,7 +1,144 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import ButtonSave from '../../components/ButtonSave';
 
 const Products = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [salePrice, setSalePrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [categoryId, setCategoryId] = useState('DEFAULT');
+
+  const [categories, setCategories] = useState([]);
+
+  const params = useParams();
+
+  useEffect(() => {
+    async function getTable() {
+      const responseCategories = await axios.get(
+        'https://localhost:7097/categories',
+      );
+      setCategories(responseCategories.data);
+    }
+    getTable();
+  }, []);
+
+  useEffect(() => {
+    if (params.id) {
+      async function getProducts() {
+        const response = await axios.get(
+          `https://localhost:7097/products/${params.id}`,
+        );
+        setName(response.data.name);
+        setSalePrice(response.data.salePrice);
+        setDescription(response.data.description);
+        setQuantity(response.data.quantity);
+        setCategoryId(response.data.category.id);
+      }
+      getProducts();
+    }
+  }, [params.id]);
+
+  async function createUpdateProducts(e) {
+    e.preventDefault();
+
+    if (!params.id) {
+      await axios.post('https://localhost:7097/products', {
+        name,
+        salePrice,
+        description,
+        quantity,
+        categoryId: Number(categoryId),
+      });
+    } else {
+      await axios.put(`https://localhost:7097/products/${params.id}`, {
+        name,
+        salePrice,
+        description,
+        quantity,
+        categoryId: Number(categoryId),
+      });
+    }
+    navigate('/products');
+  }
+
+  return (
+    <div>
+      <h1 className="ml-5 text-2xl font-bold">Produtos</h1>
+
+      <form className="m-5 w-5/6 mx-auto" onSubmit={createUpdateProducts}>
+        <label className="block mt-4" htmlFor="name">
+          Nome
+        </label>
+        <input
+          className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-300 focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        />
+
+        <label className="block mt-4" htmlFor="salePrice">
+          Preço
+        </label>
+        <input
+          className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-300 focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+          type="text"
+          name="salePrice"
+          id="salePrice"
+          value={salePrice}
+          onChange={({ target }) => setSalePrice(target.value)}
+        />
+
+        <label className="block mt-4" htmlFor="description">
+          Descrição
+        </label>
+        <input
+          className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-300 focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={({ target }) => setDescription(target.value)}
+        />
+
+        <label className="block mt-4" htmlFor="quantity">
+          Quantidade
+        </label>
+        <input
+          className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-300 focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+          type="text"
+          name="quantity"
+          id="quantity"
+          value={quantity}
+          onChange={({ target }) => setQuantity(target.value)}
+        />
+
+        <label className="block mt-4">Categoria</label>
+        <select
+          value={categoryId}
+          onChange={({ target }) => setCategoryId(target.value)}
+          className="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        >
+          <option value="DEFAULT" disabled>
+            Selecione uma categoria
+          </option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+
+        <ButtonSave />
+      </form>
+    </div>
+  );
 };
 
 export default Products;
